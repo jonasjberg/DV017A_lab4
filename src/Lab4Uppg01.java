d294a82b60a4834ae978876188370a0b4d8d9989
Jonas Sjöberg <jomeganas@gmail.com>
2015-08-07 23:34:45 +0200

    Lägg till Uppgift #1.

---
+++ src/Lab4Uppg01.java
@@ -0,0 +1,18 @@
+/**
+ * DV017A :: Grundläggande programmering i Java
+ * 860224 Jonas Sjöberg
+ * Högskolan i Gävle
+ * tel12jsg@student.hig.se
+ *
+ * Labb #4    Uppgift 1
+ */
+
+public class Lab4Uppg01
+{
+
+    public static void main(String[] args)
+    {
+        /* TODO: Uppgift 1 .. */
+    }
+
+}
